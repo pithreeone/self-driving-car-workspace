d02db5a3c29ae37ecd1f65b9cc7b317a10222369
pithreeone <pithreeone@gmail.com>
2023-11-03 15:54:57 +0800

    Feat: can demo. but not good. Problems seems to occur when prediction

--- src/fusion/src/EKF.py
+++ src/fusion/src/EKF.py
@@ -12,29 +12,38 @@ class ExtendedKalmanFilter:
         self.pose = np.zeros(6)
         
         # Transition matrix
-        self.A = np.identity(3)
-        self.B = np.identity(3)
+        self.A = np.identity(6)
+        self.B = [[1,0,0],[0,1,0],[0,0,1],[1,0,0],[0,1,0],[0,0,1]]
+        
         
         # State covariance matrix
-        self.S = np.identity(3) * 1
+        self.S = np.identity(6) * 1
         
         # Observation matrix
-        self.C = np.identity(3)
+        self.C = np.zeros((3,6))
         
         # State transition error
-        self.R = np.identity(3) * 1
+        self.R = np.identity(6) * 1
         
         # Measurement error
-        self.Q = np.identity(3) * 1
-        print("Initialize Kalman Filter")
+        self.Q = np.identity(3) * 0.00000001
+        print("Initialize Extended Kalman Filter")
     
+    def set_initial_pose(self, x, y, yaw):
+        self.pose[0] = x
+        self.pose[1] = y
+        self.pose[2] = yaw
+        
+
     def predict(self, u):
         # Base on the Kalman Filter design in Assignment 3
         # Implement a linear or nonlinear motion model for the control input
         # Calculate Jacobian matrix of the model as self.A
         
-        ???
-        raise NotImplementedError
+        self.A[0,0] = self.A[1,1] = self.A[2,2] = 1
+        self.A[3,3] = self.A[4,4] = self.A[5,5] = 0
+        self.pose = self.A.dot(self.pose) + np.matmul(self.B, u)
+        self.S = self.A @ self.S @ np.transpose(self.A) + self.R
     
         
     def update(self, z):
@@ -42,8 +51,17 @@ class ExtendedKalmanFilter:
         # Implement a linear or nonlinear observation matrix for the measurement input
         # Calculate Jacobian matrix of the matrix as self.C
         
-        ???
-        raise NotImplementedError
+        self.C = [[1,0,0,0,0,0],
+                  [0,1,0,0,0,0],
+                  [0,0,1,0,0,0]]
+        
+        den = self.C @ self.S @ np.transpose(self.C) + self.Q
+        den = np.linalg.inv(den)
+        K = self.S @ np.transpose(self.C) @ den
+
+        self.pose = self.pose + np.matmul(K, z - np.matmul(self.C, self.pose))
+        self.S = np.matmul((np.identity(len(self.pose))-np.matmul(K, self.C)), self.S)
+
         return self.pose, self.S
     
     
--- src/fusion/src/fusion.py
+++ src/fusion/src/fusion.py
@@ -18,11 +18,14 @@ class Fusion:
         rospy.Subscriber('/gt_odom', Odometry, self.gtCallback)
         rospy.on_shutdown(self.shutdown)
         self.posePub = rospy.Publisher('/pred', Odometry, queue_size = 10)
-        self.EKF = None
+        self.EKF = ExtendedKalmanFilter()
         
         self.gt_list = [[], []]
         self.est_list = [[], []]
         self.initial = False
+
+        self.prev_pose = np.zeros(3)
+        self.prev_gps = np.zeros(3)
 
     def shutdown(self):
         print("shuting down fusion.py")
@@ -37,18 +40,14 @@ class Fusion:
         
         # Change to the state yaw from EKF
         quaternion = quaternion_from_euler(0, 0, self.EKF.pose[2])
+        # print(self.EKF.pose[0], self.EKF.pose[1], self.EKF.pose[2])
         predPose.pose.pose.orientation.x = quaternion[0]
         predPose.pose.pose.orientation.y = quaternion[1]
         predPose.pose.pose.orientation.z = quaternion[2]
         predPose.pose.pose.orientation.w = quaternion[3]
         
         # Change to the covariance matrix of [x, y, yaw] from EKF
-        predPose.pose.covariance = [???, ???, 0, 0, 0, ???,
-                                    ???, ???, 0, 0, 0, ???,
-                                    0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0,
-                                    ???, ???, 0, 0, 0, ???]
+        predPose.pose.covariance = tuple(self.EKF.S.ravel().tolist())
                                     
         self.posePub.publish(predPose)
     
@@ -72,17 +71,36 @@ class Fusion:
         #     Calculate transformation matrix between 2 odometry data
         #         -> transformation = last_odom_pose^-1 * current_odom_pose
         #     etc.
-        control = ???
+        diff_x = odom_x - self.prev_pose[0]
+        diff_y = odom_y - self.prev_pose[1]
+        diff_yaw =  odom_yaw - self.prev_pose[2]
+        # set the current pose to previous pose
+        self.prev_pose[0] = odom_x
+        self.prev_pose[1] = odom_y
+        self.prev_pose[2] = odom_yaw
+
+        control = np.zeros(3)
+        control[0] = diff_x
+        control[1] = diff_y
+        control[2] = diff_yaw
         
         if not self.initial:
             self.initial = True
-            self.EKF = ExtendedKalmanFilter(odom_x, odom_y, odom_yaw)
         else:
             # Update error covriance
-            self.EKF.R = ???
-            self.EKF.predict_EKF(u = control)
+            # self.EKF.R = [[odom_covariance[0,0],0,0,0,0,0],
+            #               [0,odom_covariance[1,1],0,0,0,0],
+            #               [0,0,odom_covariance[2,2],0,0,0],
+            #               [0,0,0,0,0,0],
+            #               [0,0,0,0,0,0],
+            #               [0,0,0,0,0,0]]
+            # self.EKF.R[0,0] = odom_covariance[0,0]
+            # self.EKF.R[1,1] = odom_covariance[1,1]
+            # self.EKF.R[2,2] = odom_covariance[2,2]
+            self.EKF.predict(u = control)
 
         self.predictPublish()
+        return
         
     def gpsCallback(self, data):
         gps_x = data.pose.pose.position.x
@@ -94,16 +112,22 @@ class Fusion:
         #     Use GPS directly
         #     Find a approximate yaw
         #     etc.
-        measurement = ???
         
         if not self.initial:
-            self.initial = ExtendedKalmanFilter(gps_x, gps_y)
+            # self.initial = ExtendedKalmanFilter(gps_x, gps_y)
             self.initial = True
+            self.EKF.set_initial_pose(gps_x, gps_y, 0)
         else:
             # Update error covriance
-            self.EKF.Q = ???
+            self.EKF.Q = [[gps_covariance[0,0],0,0],
+                          [0,gps_covariance[1,1],0],
+                          [0,0,gps_covariance[0,0]]]
+            ang_approx = atan2(gps_y - self.prev_gps[1], gps_x - self.prev_gps[0])
+            measurement = [gps_x, gps_y, ang_approx]
             self.EKF.update(z = measurement)
             
+        self.prev_gps[0] = gps_x
+        self.prev_gps[1] = gps_y
         self.predictPublish()
         return
     
@@ -112,8 +136,8 @@ class Fusion:
         self.gt_list[1].append(data.pose.pose.position.y)
         if self.EKF is not None:
             # Change to the state x and state y from EKF
-            self.est_list[0].append(???) 
-            self.est_list[1].append(???)
+            self.est_list[0].append(self.EKF.pose[0])
+            self.est_list[1].append(self.EKF.pose[1])
         return
 
     def plot_path(self):
